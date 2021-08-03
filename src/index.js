76ff5e09a9861e1f959508989e50552d8aff14c9
Veranika Hrecnaya <84381787+NIKUKUs@users.noreply.github.com>
2021-08-03 16:35:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -25,12 +25,12 @@ module.exports = function toReadable (number) {
 		16: "sixteen",
 		17: "seventeen",
 		18: "eighteen",
-		19: "nineteen"
+		19: "nineteen",
+		20: "twenty"
 	};
 
 	const value3 = {
 
-		2: "twenty",
 		3: "thirty",
 		4: "forty",
 		5: "fifty",
@@ -40,20 +40,30 @@ module.exports = function toReadable (number) {
 		9: "ninety"
 	};
 
+
 	  if (number === 0) {
         return value1[0];
     }
+
+    if (number === 11) {
+    	return "eleven";
+    }
+    
 
     if (0 < number && number < 11) {
     	return value1[number];
     }
 
-    if (number === 11) {
-    	return value2[0];
+
+    if (11 < number && number < 21) {
+    	return value2[number];
     }
 
-    if (11 < number && number < 20) {
-    	return value2[number];
+
+    if (19 < number && number < 30) {
+
+	const arrnum = number.toString().split('').map(Number)
+	return value3[arrnum[0]] + " " + value1[arrnum[1]];
     };
 
 }
